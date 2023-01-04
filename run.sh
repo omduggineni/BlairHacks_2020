6c0f8df4cf611a5dd1903efd639a410d1cd41c7c
Om Duggineni <omduggineni@gmail.com>
2023-01-04 09:15:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ rm -rvf BlairHacks_2020;
 git clone https://github.com/OmDuggineni/BlairHacks_2020.git;
 cat /site/public/BlairHacks_2020/run.sh > /site/run.sh;
 cd BlairHacks_2020;
-node server.js;
+npm run start > /site/server.log;
